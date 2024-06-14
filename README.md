2f958c40ce80f6013d8d806470a7df2d7e725926
SimonLin <sadwx4017@gmail.com>
2024-06-15 01:13:18 +0800

    rename config python file

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## To Convert config to DataModel
 
-datamodel-codegen --input ./config/config.yaml --input-file-type yaml --output ./config/config.py
+datamodel-codegen --input ./config/config.yaml --input-file-type yaml --output ./config/model.py --class-name Config --disable-timestamp
--- config/loader.py
+++ config/loader.py
@@ -2,7 +2,7 @@ import pathlib
 
 from pydantic_yaml import parse_yaml_file_as
 
-from .config import Model
+from .model import Config
 
 __config_path = pathlib.Path(__file__).parent / "config.yaml"
-app_config = parse_yaml_file_as(Model, __config_path)
+app_config = parse_yaml_file_as(Config, __config_path)
